8a007c0f9083481e6bcd724f5184469606776f59
Isac Tomaz da Silva <its@cin.ufpe.br>
2021-12-21 14:52:25 -0300

    add questions

--- Health/TestModel.swift
+++ Health/TestModel.swift
@@ -10,8 +10,11 @@ import Foundation
 struct TestModel {
 
     static let questions = [
+        Question(content: "Você sente medo de ser excluído?"),
+        Question(content: "Você sente necessidade de seguir tendências de redes sociais?"),
+        Question(content: "Você sente necessidade de estar usando smartphones constantemente?"),
         Question(content: "Você sente muita falta de comprar algo?"),
-        Question(content: "Qual seu sentimento relacionado à última compra?"),
+        Question(content: "Qual seu sentimento relacionado à última compra? (Baixo para triste, e alto para feliz)"),
         Question(content: "Sua última compra foi adequada para sua necessidade?"),
         Question(content: "Sua última compra foi influenciada por amigos/redes sociais?"),
     ]
@@ -24,7 +27,7 @@ struct TestModel {
         Choice(content: "Muito alto"),
     ]
 
-    static var choice = [2, 2, 2, 2]
+    static var choice = [2, 2, 2, 2, 2, 2, 2]
 
     struct Question {
         var content: String
